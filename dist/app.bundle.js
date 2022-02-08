2df3cb236a1c33fe8ab08fc7ed6964bcc1f7bdd5
Olayinka Ola <mimoyinka@gmail.com>
2022-02-08 19:23:11 +0100

    Misc: Dist Folder

---
+++ dist/app.bundle.js
@@ -0,0 +1,43 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/client/js/main.js":
+/*!*******************************!*\
+  !*** ./src/client/js/main.js ***!
+  \*******************************/
+/***/ (() => {
+
+eval("alert(\"Working!\");\n\n//# sourceURL=webpack:///./src/client/js/main.js?");
+
+/***/ }),
+
+/***/ "./src/client/styles/main.scss":
+/*!*************************************!*\
+  !*** ./src/client/styles/main.scss ***!
+  \*************************************/
+/***/ (() => {
+
+eval("\n\n//# sourceURL=webpack:///./src/client/styles/main.scss?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/client/styles/main.scss"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/client/js/main.js"]();
+/******/ 	
+/******/ })()
+;
