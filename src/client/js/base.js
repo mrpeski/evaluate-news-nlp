98cb4eab739a786a25001b8cb04c8470e9b17e2b
Olayinka Ola <mimoyinka@gmail.com>
2022-02-09 13:20:25 +0100

    Feat: Input Validation

---
+++ src/client/js/base.js
@@ -0,0 +1,16 @@
+
+export const form = document.querySelector("#main-from");
+
+export function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(this)
+    const url = formData.get('url');
+
+    if(!url) return alert('Please fill in the article url');
+    if(!isValidURL(url)) return alert('Please enter a valid URL.')
+}
+
+function isValidURL(test){
+    return test.match(/https?:\/\/(www\.)?\w+\.\w{2,10}\/.*/);
+}
+
