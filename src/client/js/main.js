d576876b36f0194aa6e24a10c0c231cfed241ac0
Olayinka Ola <mimoyinka@gmail.com>
2022-02-09 13:23:10 +0100

    Chore: Setup Event Listeners

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,8 @@
-alert("Working!");
+import {form, handleSubmit} from "./base.js";
+
+function main() {
+	// alert('Called!');
+	form.addEventListener('submit', handleSubmit);
+}
+
+document.addEventListener('DOMContentLoaded', main);
