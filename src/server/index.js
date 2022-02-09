3bba92369fd4428e91d608f2d4dcf999662a2151
Olayinka Ola <mimoyinka@gmail.com>
2022-02-09 14:31:52 +0100

    Chore: CLean up

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import {FormData} from "formdata-node";
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 
 dotenv.config();
@@ -9,7 +9,7 @@ console.log(`Your API key is ${process.env.API_KEY}`);
 
 const formdata = new FormData();
 formdata.append("key", process.env.API_KEY);
-formdata.append("txt", "The 2022 Africa Cup of Nations is in the books and Senegal are champions for the first time in the country's history. Senegal outlasted Egypt in the final on Sunday, winning a penalty kick shootout (4-2) after the two sides were scoreless through 120 minutes. It's the first ever AFCON title for Senegal, who denied Egypt their eighth championship.\n" +
+formdata.append("url", "The 2022 Africa Cup of Nations is in the books and Senegal are champions for the first time in the country's history. Senegal outlasted Egypt in the final on Sunday, winning a penalty kick shootout (4-2) after the two sides were scoreless through 120 minutes. It's the first ever AFCON title for Senegal, who denied Egypt their eighth championship.\n" +
     "\n");
 formdata.append("lang", "en");  // 2-letter code, like en es fr ...
 
