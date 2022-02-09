f883a1777c8049d8f96f40f01feabcbab0a928f8
Olayinka Ola <mimoyinka@gmail.com>
2022-02-09 14:30:42 +0100

    Chore: Fix regex to make url last segment optional

--- src/client/js/base.js
+++ src/client/js/base.js
@@ -8,9 +8,12 @@ export function handleSubmit(e) {
 
     if(!url) return alert('Please fill in the article url');
     if(!isValidURL(url)) return alert('Please enter a valid URL.')
+
+    // Pass url to Sentiment Analysis API.
+
 }
 
 function isValidURL(test){
-    return test.match(/https?:\/\/(www\.)?\w+\.\w{2,10}\/.*/);
+    return test.match(/https?:\/\/(www\.)?\w+\.\w{2,10}(\/.*)?/g);
 }
 
