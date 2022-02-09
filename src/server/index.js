8322ef7bb75822d6a9c12653acc8c86fc4e3bc50
Olayinka Ola <mimoyinka@gmail.com>
2022-02-09 17:57:24 +0100

    Chore: Install and setup express

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,43 @@
 import dotenv from 'dotenv';
 import {FormData} from "formdata-node";
 import fetch from "node-fetch";
-
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 
-console.log(`Your API key is ${process.env.API_KEY}`);
+const app = express();
 
-const formdata = new FormData();
-formdata.append("key", process.env.API_KEY);
-formdata.append("url", "The 2022 Africa Cup of Nations is in the books and Senegal are champions for the first time in the country's history. Senegal outlasted Egypt in the final on Sunday, winning a penalty kick shootout (4-2) after the two sides were scoreless through 120 minutes. It's the first ever AFCON title for Senegal, who denied Egypt their eighth championship.\n" +
-    "\n");
-formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const requestOptions = {
+app.use(cors());
+
+app.route('/sentiment')
+    .get(function(req,res){
+        return res.json(process.env.API_KEY)
+    }).post(async function(req, res){
+        const result = await execFetch(req.body);
+        return res.json(result);
+    })
+
+app.listen(8081, function(){
+    console.log('Listening at port 8081')
+})
+
+function execFetch({url}){
+    const formdata = new FormData();
+    formdata.append("key", process.env.API_KEY);
+    formdata.append("url", url);
+    formdata.append("lang", "en");
+
+    const requestOptions = {
     method: 'POST',
     body: formdata,
     redirect: 'follow'
 };
-
-const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-    .then(response => ({
-        status: response.status,
-        body: response.json()
-    }))
-    .then(({ status, body }) => body).then(body => console.log(body))
+    return fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+    .then(response => response.json())
     .catch(error => console.log('error', error));
-
-// console.log(response)
+}
