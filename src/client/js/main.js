2275512a839ed56f3cf41ebba1529ac32a1ff3da
Olayinka Ola <mimoyinka@gmail.com>
2022-02-09 14:32:33 +0100

    Chore: ENLP test

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,8 +1,12 @@
 import {form, handleSubmit} from "./base.js";
 
 function main() {
-	// alert('Called!');
 	form.addEventListener('submit', handleSubmit);
 }
 
 document.addEventListener('DOMContentLoaded', main);
+
+const log = () => console.log(form)
+
+// Whatever is exported here will be available under ENLP;
+export { log }
