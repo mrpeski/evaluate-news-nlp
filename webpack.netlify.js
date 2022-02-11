ec92f252f16bac833a9e59e88b4f10a1afe60288
Olayinka Ola <mimoyinka@gmail.com>
2022-02-12 00:59:34 +0100

    Chore: Netlify webpack config

---
+++ webpack.netlify.js
@@ -0,0 +1,72 @@
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const sass = require("sass");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+const config = {
+    mode: 'production',
+    entry: {
+        app: [
+            path.resolve('src/client/styles/main.scss'),
+            path.resolve('src/client/js/main.js')
+        ],
+    },
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Evaluate News",
+            template: path.resolve('src/client/views/index.html'),
+            minify: true
+        }),
+        new CleanWebpackPlugin({
+            verbose: false,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false,
+        }),
+        new WorkboxPlugin.GenerateSW(),
+        new Dotenv({
+            path: './netlify.env',
+        })
+
+    ],
+    resolve: {
+        extensions: [".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: sass,
+                        },
+                    },
+                ],
+            },
+            {   test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ],
+    },
+};
+
+module.exports = config
